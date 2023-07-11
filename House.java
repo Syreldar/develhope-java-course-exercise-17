f1a4addaee2d97ca0c4a556f36479889ece5c49b
Syreldar <enrico.drago3@gmail.com>
2023-07-11 15:13:40 +0200

    Update House.java

--- House.java
+++ House.java
@@ -1,3 +1,39 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class House
 {
+    private int floorsNumber;
+    private String address;
+    private List<String> residentsNames;
+
+    public int getFloorsNumber()
+    {
+        return floorsNumber;
+    }
+
+    public void setFloorsNumber(int floorsNumber)
+    {
+        this.floorsNumber = floorsNumber;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public List<String> getResidentsNames()
+    {
+        return residentsNames;
+    }
+
+    public void setResidentsNames(String residents)
+    {
+        this.residentsNames = Arrays.asList(residents.split(","));
+    }
 }
